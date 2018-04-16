b55ad8c018c495900eaa433e13432e59dbb27d8a
Jewel <bsse0608@iit.du.ac.bd>
2018-04-16 17:44:41 +0600

    Initial commit

---
+++ app/src/main/java/ratul/jewel/nexus101/LoginActivity.java
@@ -0,0 +1,14 @@
+package ratul.jewel.nexus101;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_login);
+    }
+
+}
---
+++ app/src/main/java/ratul/jewel/nexus101/MainActivity.java
@@ -0,0 +1,53 @@
+package ratul.jewel.nexus101;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public Button button_teacher, button_student, button_admin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login();
+    }
+
+    protected void login(){
+
+        button_teacher = findViewById(R.id.button_teacher);
+        button_student = findViewById(R.id.button_student);
+        button_admin = findViewById(R.id.button_admin);
+
+        button_teacher.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+            }
+
+            });
+        button_student.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+            }
+
+        });
+        button_admin.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+            }
+
+        });
+
+    }
+}
